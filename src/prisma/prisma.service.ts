cc884616062f78d6efcb099f2d45c229d722a66e
Tiago Lopes <tiagoltavares2002@gmail.com>
2025-09-25 19:48:44 -0300

    configurando o prisma na aplicação

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from 'generated/prisma'
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({ log: ['warn', 'error'] })
+  }
+
+  onModuleInit() {
+    this.$connect()
+  }
+
+  onModuleDestroy() {
+    this.$disconnect()
+  }
+}
