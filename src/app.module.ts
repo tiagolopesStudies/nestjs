d647b1a5cf4c1e03b0608db3513f6cd188adce30
Tiago Lopes <tiagoltavares2002@gmail.com>
2025-09-26 10:07:35 -0300

    configurando as variáveis de ambiente de forma global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (config) => envSchema.parse(config)
+    })
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService]
 })
