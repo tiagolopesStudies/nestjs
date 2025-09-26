8abb9e406d11de9b6eebb4907cbd23c1f1ee7120
Tiago Lopes <tiagoltavares2002@gmail.com>
2025-09-26 12:55:06 -0300

    implementando lógica de validação do token em rotas autenticadas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
+import { JwtStrategy } from './auth/jwt.strategy'
 import { AuthenticationController } from './controllers/authentication.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from './env'
@@ -15,6 +16,6 @@ import { PrismaService } from './prisma/prisma.service'
     AuthModule
   ],
   controllers: [CreateAccountController, AuthenticationController],
-  providers: [PrismaService]
+  providers: [PrismaService, JwtStrategy]
 })
 export class AppModule {}
