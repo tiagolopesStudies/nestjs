561beb3f55764f39fdd33d11371b9285a01cbf11
Tiago Lopes <tiagoltavares2002@gmail.com>
2025-09-26 12:05:50 -0300

    ajustando a rota de autenticação

--- src/controllers/authentication.controller.ts
+++ src/controllers/authentication.controller.ts
@@ -1,13 +1,41 @@
-import { Controller, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UsePipes
+} from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import bcryptjs from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
 
-@Controller('sessions')
+const authenticationBodySchema = z.object({
+  email: z.email(),
+  password: z.string().min(6)
+})
+
+type AuthenticationBody = z.infer<typeof authenticationBodySchema>
+
+@Controller('/sessions')
+@UsePipes(new ZodValidationPipe(authenticationBodySchema))
 export class AuthenticationController {
-  constructor(private jwt: JwtService) {}
+  constructor(
+    private readonly jwt: JwtService,
+    private readonly prisma: PrismaService
+  ) {}
 
   @Post()
-  async handle() {
-    const token = await this.jwt.signAsync({ sub: 'test-id' })
-    return { token }
+  async handle(@Body() body: AuthenticationBody) {
+    const { email, password } = body
+    const user = await this.prisma.user.findUnique({ where: { email } })
+
+    if (!user || !bcryptjs.compareSync(password, user.password)) {
+      throw new UnauthorizedException('User credentials do not match')
+    }
+
+    const accessToken = this.jwt.sign({ sub: user.id })
+    return { access_token: accessToken }
   }
 }
