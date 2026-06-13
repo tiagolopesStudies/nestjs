f8d621945d51de0cedc5218e23ba1304e781c46e
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-06-13 15:42:15 -0300

    chore: melhorando a configuração do banco de dados
    - Utilizando uma URL de conexão ao invés de passar cada informação individualmente
    - Utilizando o "ConfigModule" para ler as variáveis de ambiente da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from '@/users/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT ?? '5432'),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.getOrThrow<string>('DATABASE_URL'),
+        // host: process.env.DB_HOST,
+        // port: parseInt(process.env.DB_PORT ?? '5432'),
+        // username: process.env.DB_USERNAME,
+        // password: process.env.DB_PASSWORD,
+        // database: process.env.DB_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     UserModule,
   ],
